4d2ddd3e4b58f9c097f2f0e343f0541355b23070
NazarKhandoha <khandoha0110nazar@gmail.com>
2016-07-03 15:05:14 +0300

    newtoninterpol

---
+++ Newton/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle55 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle56 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonTablucia = new System.Windows.Forms.Button();
+            this.buttonCalculatePolinom = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonCalculateAtPoint = new System.Windows.Forms.Button();
+            this.buttonFillTableFromFile = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxPolynom = new System.Windows.Forms.TextBox();
+            this.buttonFillTableWithTabulation = new System.Windows.Forms.Button();
+            this.tableInterpolationPoints = new System.Windows.Forms.DataGridView();
+            this.x = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tableInterpolationPoints)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Кількість вузлів:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(58, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "5";
+            // 
+            // buttonTablucia
+            // 
+            this.buttonTablucia.Location = new System.Drawing.Point(12, 38);
+            this.buttonTablucia.Name = "buttonTablucia";
+            this.buttonTablucia.Size = new System.Drawing.Size(157, 40);
+            this.buttonTablucia.TabIndex = 2;
+            this.buttonTablucia.Text = "Створити таблицю розділених різниць";
+            this.buttonTablucia.UseVisualStyleBackColor = true;
+            this.buttonTablucia.Click += new System.EventHandler(this.buttonCreateTable_Click);
+            // 
+            // buttonCalculatePolinom
+            // 
+            this.buttonCalculatePolinom.Location = new System.Drawing.Point(88, 84);
+            this.buttonCalculatePolinom.Name = "buttonCalculatePolinom";
+            this.buttonCalculatePolinom.Size = new System.Drawing.Size(94, 53);
+            this.buttonCalculatePolinom.TabIndex = 3;
+            this.buttonCalculatePolinom.Text = "Обчислити розділені різниці";
+            this.buttonCalculatePolinom.UseVisualStyleBackColor = true;
+            this.buttonCalculatePolinom.Click += new System.EventHandler(this.buttonCalculatePolynom_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-1, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Точка інтерполяції: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 143);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(58, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // buttonCalculateAtPoint
+            // 
+            this.buttonCalculateAtPoint.Location = new System.Drawing.Point(2, 169);
+            this.buttonCalculateAtPoint.Name = "buttonCalculateAtPoint";
+            this.buttonCalculateAtPoint.Size = new System.Drawing.Size(170, 30);
+            this.buttonCalculateAtPoint.TabIndex = 6;
+            this.buttonCalculateAtPoint.Text = "Додати точку";
+            this.buttonCalculateAtPoint.UseVisualStyleBackColor = true;
+            this.buttonCalculateAtPoint.Click += new System.EventHandler(this.buttonCalculateAtPoint_Click);
+            // 
+            // buttonFillTableFromFile
+            // 
+            this.buttonFillTableFromFile.Location = new System.Drawing.Point(2, 324);
+            this.buttonFillTableFromFile.Name = "buttonFillTableFromFile";
+            this.buttonFillTableFromFile.Size = new System.Drawing.Size(206, 30);
+            this.buttonFillTableFromFile.TabIndex = 7;
+            this.buttonFillTableFromFile.Text = "Зчитатити дані  з файлу";
+            this.buttonFillTableFromFile.UseVisualStyleBackColor = true;
+            this.buttonFillTableFromFile.Click += new System.EventHandler(this.buttonFillTableFromFile_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // textBoxPolynom
+            // 
+            this.textBoxPolynom.Location = new System.Drawing.Point(15, 267);
+            this.textBoxPolynom.Multiline = true;
+            this.textBoxPolynom.Name = "textBoxPolynom";
+            this.textBoxPolynom.ReadOnly = true;
+            this.textBoxPolynom.Size = new System.Drawing.Size(157, 41);
+            this.textBoxPolynom.TabIndex = 8;
+            this.textBoxPolynom.TextChanged += new System.EventHandler(this.textBoxPolynom_TextChanged);
+            // 
+            // buttonFillTableWithTabulation
+            // 
+            this.buttonFillTableWithTabulation.Location = new System.Drawing.Point(-2, 84);
+            this.buttonFillTableWithTabulation.Name = "buttonFillTableWithTabulation";
+            this.buttonFillTableWithTabulation.Size = new System.Drawing.Size(84, 53);
+            this.buttonFillTableWithTabulation.TabIndex = 9;
+            this.buttonFillTableWithTabulation.Text = "Табуляція функції";
+            this.buttonFillTableWithTabulation.UseVisualStyleBackColor = true;
+            this.buttonFillTableWithTabulation.Click += new System.EventHandler(this.buttonFillTableWithTabulation_Click);
+            // 
+            // tableInterpolationPoints
+            // 
+            this.tableInterpolationPoints.AllowUserToAddRows = false;
+            this.tableInterpolationPoints.AllowUserToDeleteRows = false;
+            this.tableInterpolationPoints.AllowUserToResizeColumns = false;
+            this.tableInterpolationPoints.AllowUserToResizeRows = false;
+            this.tableInterpolationPoints.BackgroundColor = System.Drawing.SystemColors.ScrollBar;
+            this.tableInterpolationPoints.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableInterpolationPoints.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.x,
+            this.y,
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.tableInterpolationPoints.Location = new System.Drawing.Point(481, 32);
+            this.tableInterpolationPoints.Name = "tableInterpolationPoints";
+            this.tableInterpolationPoints.ReadOnly = true;
+            this.tableInterpolationPoints.RowHeadersVisible = false;
+            this.tableInterpolationPoints.Size = new System.Drawing.Size(461, 237);
+            this.tableInterpolationPoints.TabIndex = 10;
+            // 
+            // x
+            // 
+            this.x.Frozen = true;
+            this.x.HeaderText = "x";
+            this.x.Name = "x";
+            this.x.ReadOnly = true;
+            this.x.Width = 50;
+            // 
+            // y
+            // 
+            this.y.Frozen = true;
+            this.y.HeaderText = "L(x)";
+            this.y.Name = "y";
+            this.y.ReadOnly = true;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "F(x)";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle55.Format = "E4";
+            dataGridViewCellStyle55.NullValue = null;
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle55;
+            this.Column2.HeaderText = "W(x)";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            dataGridViewCellStyle56.Format = "E4";
+            dataGridViewCellStyle56.NullValue = null;
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle56;
+            this.Column3.HeaderText = "| F(x) - L(x) |";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(941, 384);
+            this.Controls.Add(this.tableInterpolationPoints);
+            this.Controls.Add(this.buttonFillTableWithTabulation);
+            this.Controls.Add(this.textBoxPolynom);
+            this.Controls.Add(this.buttonFillTableFromFile);
+            this.Controls.Add(this.buttonCalculateAtPoint);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonCalculatePolinom);
+            this.Controls.Add(this.buttonTablucia);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Інтерполяція функцій многочленом Ньютона";
+            ((System.ComponentModel.ISupportInitialize)(this.tableInterpolationPoints)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonTablucia;
+        private System.Windows.Forms.Button buttonCalculatePolinom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonCalculateAtPoint;
+        private System.Windows.Forms.Button buttonFillTableFromFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox textBoxPolynom;
+        private System.Windows.Forms.Button buttonFillTableWithTabulation;
+        private System.Windows.Forms.DataGridView tableInterpolationPoints;
+        private System.Windows.Forms.DataGridViewTextBoxColumn x;
+        private System.Windows.Forms.DataGridViewTextBoxColumn y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ Newton/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Поля
+
+        private static int Degree;
+        private double maxf;
+        private static DataGridView XData;
+        private static DataGridView[] YData;
+
+        private static double[] XArray;
+        private static double[][] YArray;
+
+        #endregion
+
+        private void InitializeDataContainers()//контейнер для розділених різниць
+        {
+            if (XData != null)
+            
+                XData.Dispose();
+
+            if (YData != null)
+                foreach (DataGridView NewData in YData)
+                {
+                    NewData.Dispose();
+                }
+
+            XData = new DataGridView();
+            YData = new DataGridView[Degree];
+
+            XArray = new double[Degree];
+            YArray = new double[Degree][];
+
+            for (int i = 0; i < YArray.Length; i++)
+            {
+                YArray[i] = new double[Degree - i];
+            }
+
+        }
+        private void InitializeDataGridView(DataGridView data, int rowCount, Point location)//cтворення таблиці
+        {
+            data.Size = new Size(50, rowCount * 22);
+            data.Location = location;
+            data.TabIndex = 0;
+
+            data.Columns.Add("Value", "");
+            data.Columns["Value"].Width = 47;
+            data.RowCount = rowCount + 1;
+            data.RowHeadersVisible = false;//заголовки стопців
+            data.ColumnHeadersVisible = false;//заголовки стопців
+
+            data.ScrollBars = ScrollBars.None;//прокрутка
+            
+            
+            //Стрічки
+            data.AllowUserToAddRows = false;
+            data.AllowUserToDeleteRows = false;
+            data.AllowUserToOrderColumns = false;
+            data.AllowUserToResizeColumns = false;
+            data.AllowUserToResizeRows = false;
+            data.ReadOnly = true;//комірки таблиці
+        }
+        private void CheckFormSizes()//визначатиме розміри нашої форми
+        {
+            Width = 800 + XData.Width + Degree * 47;
+
+            textBoxPolynom.Width = Width - 485;
+
+            if (XData.Height > 200)
+            {
+                Height = 95 + XData.Height;
+                textBoxPolynom.Location = new Point(12, 20 + XData.Height);
+            }
+            else
+            {
+                Height = 450;
+                textBoxPolynom.Location = new Point(12, 200);
+            }
+
+            tableInterpolationPoints.Location = new Point(Width - 470, tableInterpolationPoints.Location.Y);
+            tableInterpolationPoints.Height = Height - 57;
+        }
+
+        private void buttonCreateTable_Click(object sender, EventArgs e)
+        {
+            Degree = Convert.ToInt32(textBox1.Text);
+
+            InitializeDataContainers();
+
+            InitializeDataGridView(XData, Degree, new Point(196, 12));
+            Controls.Add(XData);
+
+            for (int i = 0; i < Degree; i++)
+            {
+                YData[i] = new DataGridView();
+
+                InitializeDataGridView(YData[i], Degree - i, new Point(208 + XData.Width + i * 47, 12 + i * 11));
+
+                Controls.Add(YData[i]);
+            }
+            
+            textBoxPolynom.Text = String.Empty;
+
+            CheckFormSizes();
+        }
+      
+        #region Заповнення таблиці розділених різниць
+
+        private void buttonFillTableFromFile_Click(object sender, EventArgs e)// читання з файлу таблиці розділених різниць
+        {
+            string Path;
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Path = openFileDialog.FileName;
+                string[] Data;
+
+                using (StreamReader Reader = new StreamReader(Path))
+                {
+                    Data = Reader.ReadToEnd().Split(new char[] { ' ', '\n' });
+                }
+
+                for (int i = 0; i < Degree; i++)
+                {
+                    XArray[i] = Convert.ToDouble(Data[i * 2]);
+                    YArray[0][i] = Convert.ToDouble(Data[i * 2 + 1]);
+
+                    XData[0, i].Value = Data[i * 2];
+                    YData[0][0, i].Value = Data[i * 2 + 1];
+                }
+            }
+        }
+        private void buttonFillTableWithTabulation_Click(object sender, EventArgs e)
+        {
+           
+
+            // ПРОГРАМНЕ ЗАДАННЯ 
+            int i = 0;//лічильник
+            int a = 1, b = 4;//межі нашої функції
+            double r = Math.Abs(b - a);
+            double d = Degree;
+            double h = r / d;//крок
+            maxf = maxabsderivf(a, Degree);
+            for (double j = a; i < d; j += h)
+            {
+                XArray[i] = j;
+                YArray[0][i] = f(j);
+
+                XData[0, i].Value = j;
+                YData[0][0, i].Value = f(j);
+
+                if (f(j) > maxf) maxf = f(j);
+                                
+                i++;
+            }
+        }
+
+        #endregion
+
+        private void buttonCalculatePolynom_Click(object sender, EventArgs e)//обчислення многочлена
+        {
+            for (int i = 1; i < Degree; i++)
+            {
+                for (int j = 0; j < YData[i].RowCount; j++)
+                {
+                    YArray[i][j] = (YArray[i - 1][j + 1] - YArray[i - 1][j]) / (XArray[i + j] - XArray[j]);
+                    YData[i][0, j].Value = YArray[i][j];
+                }
+            }
+
+            textBoxPolynom.Text = String.Empty;
+
+            CreateStringHornerPolynom(3);
+           // CreateStringNewtonPolynom(3);
+        }
+
+        #region Формування многочлена
+
+        private void CreateStringHornerPolynom(int roundDecimals)// Горнер
+        {
+            textBoxPolynom.Text = "L(x) = ";
+            if (YArray[0][0] != 0) textBoxPolynom.Text += YArray[0][0] + " + ";
+
+            for (int i = 0; i < Degree - 1; i++)
+            {
+                if (XArray[i] != 0)
+                    textBoxPolynom.Text += "(x-" + XArray[i] + ")(";
+                else textBoxPolynom.Text += "x(";
+
+                if (YArray[i+1][0] != 0)
+                {
+                    if (YArray[i+1][0] < 0)
+                        textBoxPolynom.Text += "-";
+                    textBoxPolynom.Text += Math.Round(Math.Abs(YArray[i+1][0]), roundDecimals) + " + ";
+                }
+            }
+            
+            for (int i = 0; i < Degree - 1; i++) textBoxPolynom.Text += ")";
+
+            textBoxPolynom.Text = textBoxPolynom.Text.Replace(" + )", ")");
+            textBoxPolynom.Text = textBoxPolynom.Text.Replace("()","");
+
+            for (int i = Degree - 1; i > 0; i--)
+            {
+                string Expression = "(x-" + XArray[i] + ")";
+
+                if (textBoxPolynom.Text.Contains(Expression + ")"))
+                {
+                    textBoxPolynom.Text = textBoxPolynom.Text.Replace(Expression, "");
+                    textBoxPolynom.Text = textBoxPolynom.Text.Replace(" + )", ")");
+                    textBoxPolynom.Text = textBoxPolynom.Text.Replace("()", "");
+                }
+            }
+        }
+        private void CreateStringNewtonPolynom(int roundDecimals)//Ньютон
+        {
+            textBoxPolynom.Text = "L(x) = ";
+            if (YArray[0][0] != 0) textBoxPolynom.Text += YArray[0][0] + " ";
+
+            for (int i = 1; i < Degree; i++)
+            {
+                if (YArray[i][0] != 0)
+                {
+                    if (YArray[i][0] > 0)
+                        textBoxPolynom.Text += "+ ";
+                    else 
+                        textBoxPolynom.Text += "- ";
+                    if (Math.Round(Math.Abs(YArray[i][0]), roundDecimals) != 1)
+                        textBoxPolynom.Text += Math.Round(Math.Abs(YArray[i][0]), roundDecimals);
+
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (XArray[j] != 0)
+                            textBoxPolynom.Text += "(x-" + XArray[j] + ")";
+                        else textBoxPolynom.Text += "x";
+                    }
+                }
+                textBoxPolynom.Text += " ";
+            }
+
+            textBoxPolynom.Text = textBoxPolynom.Text.Replace("--", "+");
+
+            if (textBoxPolynom.Text.IndexOf('+') != -1)
+            {
+                if (textBoxPolynom.Text.IndexOf('+') < textBoxPolynom.Text.IndexOfAny("123456789".ToCharArray()))
+                {
+                    textBoxPolynom.Text = textBoxPolynom.Text.Remove(textBoxPolynom.Text.IndexOf('+'), 2);
+                }
+                else if (textBoxPolynom.Text.IndexOfAny("123456789".ToCharArray()) == -1)
+                {
+                    textBoxPolynom.Text = textBoxPolynom.Text.Remove(textBoxPolynom.Text.IndexOf('+'), 2);
+                }
+            }          
+        }
+
+        #endregion        
+
+        private void buttonCalculateAtPoint_Click(object sender, EventArgs e)//обчислення і запис у таблицю
+        {
+            double InterpolationPoint = Convert.ToDouble(textBox2.Text);
+            double Result = 0;
+
+            for (int i = Degree - 1; i > 0; i--)
+            {
+                Result += YArray[i][0];
+                Result *= (InterpolationPoint - XArray[i - 1]);
+            }
+            Result += YArray[0][0];
+
+            tableInterpolationPoints.Rows.Add();
+            tableInterpolationPoints[0, tableInterpolationPoints.RowCount - 1].Value = InterpolationPoint;
+            tableInterpolationPoints[1, tableInterpolationPoints.RowCount - 1].Value = Result;
+            tableInterpolationPoints[2, tableInterpolationPoints.RowCount - 1].Value = f(InterpolationPoint);
+            tableInterpolationPoints[3, tableInterpolationPoints.RowCount - 1].Value = maxf * omega(InterpolationPoint,Degree);
+            tableInterpolationPoints[4, tableInterpolationPoints.RowCount - 1].Value = Math.Abs(f(InterpolationPoint)-Result);
+            
+        }
+
+        double f(double x)//Ф-ція
+        {
+            return  2*Math.Pow(x,5)+Math.Sin(7*x) ;
+        }
+
+        double maxabsderivf(double x, int n)//n-тта похідна
+        {
+            return Math.Pow(7,n+1) ;
+        }
+
+        double omega(double x, int n)//ф-ція омега
+        {
+            double result = 1;
+            for (int i = 0; i <= n; i++)
+            {
+                result *= (x - XArray[i]) ;
+            }
+            return Math.Abs(result);
+        }
+
+        private void textBoxPolynom_TextChanged(object sender, EventArgs e)
+        {
+
+        } 
+    }
+}
